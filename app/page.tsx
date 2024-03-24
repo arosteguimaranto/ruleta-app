b413c6d2b6459c75017f88b6cd6b4f7f3191052a
arosteguimaranto <arosteguimaranto@outlook.com>
2024-03-24 01:56:21 -0600

    supabase settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,13 @@
 import Image from "next/image";
+import Form from "@/components/form";
 
 export default function Home() {
   return (
-    <main className=" p-4 bg-white ">
+    <main className="  p-4 text-center bg-white ">
      {/*Titulo*/}
-        <h1 className=" text-3xl mb-4"> Formulario de Preguntas </h1>
+        <h1 className=" text-5xl font-bold    mb-4"> Formulario de Preguntas </h1>
      {/*Formulario*/}
-      <form>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="pregunta">
-            Ingresa la pregunta
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="Ejemplo: Que dia naci?"
-          />
-           <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="respuesta">
-           Ingresa la respuesta
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full mb-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            type="text"
-            placeholder="Ejemplo: 12 de Junio del 2000"
-          />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Enviar
-          </button>
-        </div>
-      </form>
+     <Form/>
 
     
     </main>
---
+++ components/form.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Form() {
+  
+  const { data, error } = await supabase
+  .from('Preguntas')
+  .insert([
+    { some_column: 'someValue' },
+    { some_column: 'otherValue' },
+  ])
+  .select()
+
+  return (
+    <div>
+       <form>
+        <div className=" justify-center mb-4">
+          <label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="pregunta">
+            Ingresa la pregunta
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Ejemplo: Que dia naci?"
+          />
+           <label className="block text-gray-700 text-2xl font-bold mb-4" htmlFor="respuesta">
+           Ingresa la respuesta
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full mb-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Ejemplo: 12 de Junio del 2000"
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
